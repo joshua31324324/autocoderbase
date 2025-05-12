fadbc729140b0a74704e1d22306feebcc3504465
joshua <jo336@njit.edu>
2025-05-12 15:03:10 -0400

    Update page.tsx

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -6,19 +6,27 @@ export default function Projects() {
       <main className="max-w-2xl w-full space-y-8 py-20">
         <h1 className="text-gradient-brand text-center">Our Work</h1>
         <p className="text-lg text-medium-contrast text-center">
-          Explore some of the projects we’ve worked on, each focused on delivering practical, effective solutions for real people and organizations.
+          Explore some of the projects I've made, each focused on delivering practical, effective solutions for real people and organizations.
         </p>
-        <h2 className="text-lg text-medium-contrast text-center">Unemployment</h2>
-        <a href="https://unemployment-visualization-gih01tf2q.vercel.app/">Unemployment</a>
-        <p className="text-lg text-medium-contrast text-center">
-          This site shows a slideshow showing the rates of unemployment in America.</p>
-        <h2 className="text-lg text-medium-contrast text-center">Chat</h2>
-        <p className="text-lg text-medium-contrast text-center">
-          This site is a chat application that allows users to send and receive messages in real-time.
-        </p>
-        <h2 className="text-lg text-medium-contrast text-center">Weekdays</h2>
-        <a href="https://weekdays-nine.vercel.app/">Weekdays</a>
-        <p>This site tells you what day it is today.</p>
+        <a href="https://unemployment-visualization-gih01tf2q.vercel.app/">
+    <h2 className="text-lg text-medium-contrast text-center">Unemployment</h2>
+</a>
+<p className="text-lg text-medium-contrast text-center">
+    This site shows a slideshow showing the rates of unemployment in America.
+</p>
+
+<a href="https://chat-mu-tawny-31.vercel.app/">
+    <h2 className="text-lg text-medium-contrast text-center">Chat</h2>
+</a>
+<p className="text-lg text-medium-contrast text-center">
+    This site is a chat application that allows users to send and receive messages in real-time.
+</p>
+
+<a href="https://weekdays-nine.vercel.app/">
+    <h2 className="text-lg text-medium-contrast text-center">Weekdays</h2>
+</a>
+<p>This site tells you what day it is today.</p>
+
         <div className="text-center">
           <Link href="/" className="text-primary underline hover:text-primary-700">Back to Home</Link>
         </div>
