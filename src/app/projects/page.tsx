cb7f1c73c1fcdeb39403425840243f8c67c83bbb
joshua <jo336@njit.edu>
2025-05-12 14:57:18 -0400

    added links to project

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -8,6 +8,17 @@ export default function Projects() {
         <p className="text-lg text-medium-contrast text-center">
           Explore some of the projects we’ve worked on, each focused on delivering practical, effective solutions for real people and organizations.
         </p>
+        <h2 className="text-lg text-medium-contrast text-center">Unemployment</h2>
+        <a href="https://unemployment-visualization-gih01tf2q.vercel.app/">Unemployment</a>
+        <p className="text-lg text-medium-contrast text-center">
+          This site shows a slideshow showing the rates of unemployment in America.</p>
+        <h2 className="text-lg text-medium-contrast text-center">Chat</h2>
+        <p className="text-lg text-medium-contrast text-center">
+          This site is a chat application that allows users to send and receive messages in real-time.
+        </p>
+        <h2 className="text-lg text-medium-contrast text-center">Weekdays</h2>
+        <a href="https://weekdays-nine.vercel.app/">Weekdays</a>
+        <p>This site tells you what day it is today.</p>
         <div className="text-center">
           <Link href="/" className="text-primary underline hover:text-primary-700">Back to Home</Link>
         </div>
